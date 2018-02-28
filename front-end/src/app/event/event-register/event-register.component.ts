2f98e87410515147699622daa454f7ee5262e15c
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 16:42:17 -0400

    updated event-register-from and component

--- front-end/src/app/event/event-register/event-register.component.ts
+++ front-end/src/app/event/event-register/event-register.component.ts
@@ -1,4 +1,8 @@
+import { EventService } from './../event.service';
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../category.service';
+import { Observable } from 'rxjs/Observable';
+import { EventInterface } from '../EventInterface';
 
 @Component({
   selector: 'app-event-register',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-register.component.css']
 })
 export class EventRegisterComponent implements OnInit {
+  event = {
+    'category_id': '',
+    'event_name': '',
+    'user_id': '',
+    'description': '',
+    'image': '',
+    'start_date': '',
+    'end_date': '',
+    'modified': '',
+    'start_time': '',
+    'end_time': '',
+    'location': '',
+    'count': 0,
+    'aproved': true,
+  } as EventInterface;
 
-  constructor() { }
+  categories: Observable<{'id', 'title', 'description', 'image'}[]>;
+
+  constructor(private _categoryService: CategoryService, private _eventService: EventService) { }
 
   ngOnInit() {
+    this.categories = this._categoryService.get();
+  }
+
+  onSubmit() {
+    console.log('submit event');
+    this._eventService.post(this.event).subscribe(console.log, console.log, console.log);
   }
 
 }
