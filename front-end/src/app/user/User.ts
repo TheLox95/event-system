ed535670e32822c36ee81ddab6c368e22b84dfb1
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 14:57:54 -0400

    added properties to class

--- front-end/src/app/user/User.ts
+++ front-end/src/app/user/User.ts
@@ -1,3 +1,9 @@
 export default class User {
-  constructor(public username: string, public firstName: string, public lastName: string, public readonly _id?) {}
+  constructor(
+    public username?: string,
+    public firstName?: string,
+    public lastName?: string,
+    public readonly _id?,
+    public password?: string
+  ) {}
 }
