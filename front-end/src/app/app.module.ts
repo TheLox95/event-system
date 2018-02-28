6a7e4da93cc6d921f617a8091d8bfc6ca782dd84
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 14:55:34 -0400

    added new components and reanamed others

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,24 +6,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './user/user.service';
 import { GuardService } from './auth/guard.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TokenInterceptor } from './token-interceptor';
+import { EventFormComponent } from './dashboard/event-form/event-form.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { EventRegisterComponent } from './event/event-register/event-register.component';
 
 const appRoutes: Routes = [
-  { path: '', component: UserFormComponent },
-  { path: 'panel', component: DashboardComponent, canActivate: [GuardService] }
+  { path: '', component: UserLoginComponent },
+  { path: 'register', component: UserRegisterComponent },
+  { path: 'panel', component: DashboardComponent, canActivate: [GuardService] },
+  { path: 'newEvent', component: EventFormComponent, canActivate: [GuardService] }
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent,
-    DashboardComponent
+    UserLoginComponent,
+    DashboardComponent,
+    EventFormComponent,
+    UserRegisterComponent,
+    EventRegisterComponent
   ],
   imports: [
     FormsModule,
