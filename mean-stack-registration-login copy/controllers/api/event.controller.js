9593721562d75e3e18d91d1d8e2c01cef8d9eca2
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 15:00:11 -0400

    created new event api end point

---
+++ mean-stack-registration-login copy/controllers/api/event.controller.js
@@ -0,0 +1,53 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var eventService = require('services/event.service');
+
+// routes
+router.post('/', createEvent);
+router.put('/', updateEvent);
+router.delete('/', deleteEvent);
+
+module.exports = router;
+
+function createEvent(req, res) {
+    eventService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateEvent(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // can only update own account
+        return res.status(401).send('You can only update your own account');
+    }
+
+    eventService.update(userId, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteEvent(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // can only delete own account
+        return res.status(401).send('You can only delete your own account');
+    }
+
+    eventService.delete(userId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
