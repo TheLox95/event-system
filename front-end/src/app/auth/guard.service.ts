f17a49866884da380cec14c7d11a8bcd2271589e
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 09:43:20 -0400

    now redirect to default path when user is no autenticated

--- front-end/src/app/auth/guard.service.ts
+++ front-end/src/app/auth/guard.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { TokenService } from './token.service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GuardService implements CanActivate {
 
-  constructor(private _tokenService: TokenService) { }
+  constructor(private _tokenService: TokenService, private _router: Router) { }
 
   canActivate() {
     console.log('AuthGuard#canActivate called');
-    return this._tokenService.get() === null ? false : true;
+    if (this._tokenService.get() === null) {
+      this._router.navigate(['/']);
+    }
+    return true;
   }
 
 }
