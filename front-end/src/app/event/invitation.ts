c06ff1680b47f37b7eaa122e4aee034bb60849bd
TheLox95 <leo_9_t@hotmail.com>
2018-03-01 18:50:42 -0400

    added string name to IsGoingState enum and id property to Invitation class

--- front-end/src/app/event/invitation.ts
+++ front-end/src/app/event/invitation.ts
@@ -1,5 +1,11 @@
 import { EventInterface } from './EventInterface';
 import User from '../user/User';
 export class Invitation {
-  constructor(public user: User, public event: EventInterface, public is_going: boolean) {}
+  constructor(public user: User, public event: EventInterface, public is_going: IsGoingState, public _id?: string) {}
+}
+
+export enum IsGoingState {
+  ACCEPTED = 'accepted',
+  DECLINED = 'declined',
+  UNRESPONDED = 'unresponded',
 }
