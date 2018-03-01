47a44b246f6affee0af6f1ad13d8bbf5924ba59e
TheLox95 <leo_9_t@hotmail.com>
2018-02-28 20:41:11 -0400

    added method to retrieve all events of current user

--- front-end/src/app/event/event.service.ts
+++ front-end/src/app/event/event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EventInterface } from './EventInterface';
+import User from '../user/User';
 
 @Injectable()
 export class EventService {
@@ -10,6 +11,10 @@ export class EventService {
   post(event: EventInterface) {
     console.log(event);
     return this._http.post(`http://localhost:3000/api/events/new`, event);
+  }
+
+  get(user: User) {
+    return this._http.get(`http://localhost:3000/api/events/${user._id}`).map((res: {body: any}) => (res.body));
   }
 
 }
