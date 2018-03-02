b5e0e789e2798c6c8356e91ee007c242e24ea231
TheLox95 <leo_9_t@hotmail.com>
2018-03-02 16:36:48 -0400

    now images are save to new directory and directory is created if not exist

--- mean-stack-registration-login copy/services/image.service.js
+++ mean-stack-registration-login copy/services/image.service.js
@@ -1,19 +1,46 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 var service = {};
 
 service.save = handleImageUpload;
+const route = `./uploads/images/`;
 
 module.exports = service;
 
 function handleImageUpload(image, eventObj){
 
     return new Promise((resolve, rejected) =>{
-        image.mv(`./public/images/${eventObj.event_name}.${image.name.split(".")[1]}`, function (err) {
+        makeDirectory(route)
+        .catch(rejected)
+        .then(() => {
+            return saveImage(image, eventObj, route);
+        })
+        .catch(rejected)
+        .then(resolve);
+        
+    });
+
+}
+
+function saveImage(image, eventObj, route) {
+    return new Promise((resolve, rejected) => {
+        image.mv(`${route}${eventObj.event_name}.${image.name.split(".")[1]}`, function (err) {
             if (err){
                 rejected(err);
             }
-
+    
             resolve(image.name)
         });
     });
+}
 
+function makeDirectory(path) {
+    return new Promise((resolve, rejected) => {
+        mkdirp(path, function(err) { 
+            if(err){
+                rejected(err)
+            }
+            resolve();        
+        });
+    });
 }
