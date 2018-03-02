6e7412113a561352c4db86362df00396c23cde84
TheLox95 <leo_9_t@hotmail.com>
2018-03-02 16:43:33 -0400

    new method to get event image from server

--- front-end/src/app/event/event.service.ts
+++ front-end/src/app/event/event.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { EventInterface } from './EventInterface';
 import User from '../user/User';
 import { Invitation, IsGoingState } from './invitation';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
 @Injectable()
 export class EventService {
@@ -11,8 +13,37 @@ export class EventService {
 
   post(event: EventInterface) {
     console.log(event);
-    return this._http.post(`http://localhost:3000/api/events/new`, event);
+    const formData: FormData = new FormData();
+
+    if (event.image instanceof File) {
+      formData.append('fileKey', event.image, event.image.name);
+    }
+    formData.append('event', JSON.stringify(event));
+    return this._http.post(`http://localhost:3000/api/events/new`, formData);
   }
+
+  image(event_name: string) {
+    return new Observable((observer: Subscriber<string>) => {
+      let objectUrl: string = null;
+
+      this._http
+            .get(`http://localhost:3000/api/events/image/${event_name}`, {
+                responseType: 'blob'
+            })
+          .subscribe(m => {
+              objectUrl = URL.createObjectURL(m);
+              observer.next(objectUrl);
+          });
+
+      return () => {
+          if (objectUrl) {
+              URL.revokeObjectURL(objectUrl);
+              objectUrl = null;
+          }
+      };
+  });
+
+}
 
   get(user: User) {
     return this._http.get(`http://localhost:3000/api/events/${user._id}`);
