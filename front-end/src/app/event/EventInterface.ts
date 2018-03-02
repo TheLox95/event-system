cffc6af4bda6e6cc71654db83288f371e42570b0
TheLox95 <leo_9_t@hotmail.com>
2018-03-02 16:41:22 -0400

    created category interface and added to event interface; image on event interface now is string and File object

--- front-end/src/app/event/EventInterface.ts
+++ front-end/src/app/event/EventInterface.ts
@@ -1,13 +1,15 @@
 import { Invitation } from './invitation';
 import LocationInterface from './LocationInterface';
+import Category from './CategoryInterface';
 
 export interface EventInterface {
     _id: number;
     category_id: string;
+    category: Category;
     event_name: string;
     user_id: string;
     description: string;
-    image: string;
+    image: string | File;
     start_date: string;
     end_date: string;
     modified: string;
