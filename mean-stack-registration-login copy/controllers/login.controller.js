052d38dbd99794de76c7e17544b770249a29dfe0
TheLox95 <leo_9_t@hotmail.com>
2018-03-04 11:34:49 -0400

    added handle mongo error connection

--- mean-stack-registration-login copy/controllers/login.controller.js
+++ mean-stack-registration-login copy/controllers/login.controller.js
@@ -21,7 +21,12 @@ router.post('/', function (req, res) {
         form: req.body,
         json: true
     }, function (error, response, body) {
-        console.log(body);
+        
+        if(body.substring(0, 10) == "MongoError"){
+            res.setHeader('Content-Type', 'application/json');
+            res.send({ error: true, success: false, body});
+            return
+        }
         if (error) {
             res.setHeader('Content-Type', 'application/json');
             res.send({ error: 'An error occurred' });
