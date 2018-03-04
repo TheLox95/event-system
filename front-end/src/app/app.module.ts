ce3562423d1710f3c1721d99b608fe06c72760e1
TheLox95 <leo_9_t@hotmail.com>
2018-03-04 09:18:42 -0400

    fix on imports; added AgmCoreModule

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,37 +1,37 @@
-import { EventService } from './event/event.service';
-import { TokenService } from './auth/token.service';
-import {Routes, RouterModule} from '@angular/router';
+import { EventEditComponent } from './event/event-edit/event-edit.component';
+import { CategoryService } from './event/category.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PanelComponent } from './dashboard/panel/panel.component';
+import { UserService } from './user/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UserLoginComponent } from './user/user-login/user-login.component';
-import { FormsModule } from '@angular/forms';
-import { UserService } from './user/user.service';
+import { TokenService } from './auth/token.service';
 import { GuardService } from './auth/guard.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TokenInterceptor } from './token-interceptor';
-import { UserRegisterComponent } from './user/user-register/user-register.component';
-import { EventRegisterComponent } from './event/event-register/event-register.component';
-import { CategoryService } from './event/category.service';
-import {AngularGooglePlaceModule} from 'angular-google-place';
-import { EventDetailComponent } from './event/event-detail/event-detail.component';
-import { PanelComponent } from './dashboard/panel/panel.component';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
-import { EventEditComponent } from './event/event-edit/event-edit.component';
 import { GoogleMapComponent } from './google-map/google-map.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { EventService } from './event/event.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RegisterComponent } from './event/register/register.component';
+import { EventDetailComponent } from './event/event-detail/event-detail.component';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { TokenInterceptor } from './token-interceptor';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule } from '@agm/core';
 
 const appRoutes: Routes = [
   { path: '', component: UserLoginComponent },
   { path: 'register', component: UserRegisterComponent },
   { path: 'panel', component: DashboardComponent, canActivate: [GuardService], children: [
     {path: '', component: PanelComponent },
-    { path: 'newEvent', component: EventRegisterComponent },
+    { path: 'newEvent', component: RegisterComponent },
     { path: 'eventDetail', component: EventDetailComponent },
     { path: 'eventEdit', component: EventEditComponent }
   ] }
@@ -41,30 +41,33 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent,
-    DashboardComponent,
-    UserRegisterComponent,
-    EventRegisterComponent,
-    EventDetailComponent,
-    PanelComponent,
     CapitalizePipe,
+    GoogleMapComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    PanelComponent,
+    DashboardComponent,
+    RegisterComponent,
     EventEditComponent,
-    GoogleMapComponent
+    EventDetailComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
     HttpClientModule,
-    AngularGooglePlaceModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
     BrowserAnimationsModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAGE_kW-1BQCRZtfgeou-D8-7U1qdUrVTE',
+      libraries: ['places']
+    }),
     NgbModule.forRoot(),
     RouterModule.forRoot(
       appRoutes
     )
   ],
-  providers: [UserService, GuardService, TokenService, CategoryService, EventService, {
+  providers: [TokenService, GuardService, UserService, CategoryService, EventService, {
     provide: HTTP_INTERCEPTORS,
     useClass: TokenInterceptor,
     multi: true
